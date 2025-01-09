eebd450d7d306a50d25009cde44584e6002cedce
Mathew Thompson <mathewthompson@Mathews-MacBook-Pro.local>
2025-01-09 13:23:22 -0600

    Added Type Guard Generators

---
+++ src/generators/type_guard_generator.test.ts
@@ -0,0 +1,177 @@
+import { isBoolean, isNumber, isString } from '../typeguards/scalars';
+import { makeTypeGuard, makeTypeGuardTest } from './type_guard_generator';
+
+describe('makeTypeGuard', () => {
+  test('makes a typeguard for the input', () => {
+    interface MyFunInterface {
+      key: string;
+      num: number;
+      bool: boolean;
+    }
+    const tg = makeTypeGuard<MyFunInterface>({
+      key: isString,
+      num: isNumber,
+      bool: isBoolean,
+    });
+
+    expect(typeof tg).toBe('function');
+
+    expect(tg({ key: 'hello', num: 1, bool: true })).toBe(true);
+    expect(tg({ key: 'hello', num: 1, bool: 'true' })).toBe(false);
+  });
+
+  test('a typeguard generated can be used in a nested interface', () => {
+    interface ValueSet {
+      value: string;
+      isTrue: boolean;
+    }
+
+    interface MyFunInterface {
+      key: string;
+      num: number;
+      bool: boolean;
+      valueSet: ValueSet;
+    }
+
+    const vsTG = makeTypeGuard<ValueSet>({
+      value: isString,
+      isTrue: isBoolean,
+    });
+
+    const mfiTG = makeTypeGuard<MyFunInterface>({
+      key: isString,
+      num: isNumber,
+      bool: isBoolean,
+      valueSet: vsTG,
+    });
+
+    const trueVal = {
+      key: 'key',
+      num: 96,
+      bool: false,
+      valueSet: { value: 'hello', isTrue: true },
+    };
+
+    const falseVal = {
+      key: 'key',
+      num: 96,
+      bool: false,
+      valueSet: { value: 'hello', isTrue: 'true' },
+    };
+
+    expect(mfiTG(trueVal)).toBe(true);
+    expect(mfiTG(falseVal)).toBe(false);
+  });
+
+  test('the value is correctly typeguarded', () => {
+    interface MyFunInterface {
+      key: string;
+      num: number;
+      bool: boolean;
+    }
+    const tg = makeTypeGuard<MyFunInterface>({
+      key: isString,
+      num: isNumber,
+      bool: isBoolean,
+    });
+
+    const myVal = {
+      key: 'hello',
+      num: 1,
+      bool: true,
+    };
+
+    const myFunc = (input: MyFunInterface) => (input ? true : false);
+
+    if (tg(myVal)) {
+      expect(myFunc(myVal)).toBe(true);
+    }
+
+    expect.assertions(1);
+  });
+});
+
+describe('makeTypeGuardTest', () => {
+  test('makes a typeguard test for the input', () => {
+    const tgTest = makeTypeGuardTest({
+      key: isString,
+      num: isNumber,
+      bool: isBoolean,
+    });
+
+    expect(tgTest('string')).toEqual(['root']);
+    expect(tgTest({ key: 'hello', num: 1, bool: true })).toEqual([]);
+    expect(tgTest({ key: 1, num: 1, bool: true })).toEqual(['key']);
+    expect(tgTest({ key: 'hello', num: 1, bool: 'true' })).toEqual(['bool']);
+    expect(tgTest({ key: 'hello', num: '1', bool: true })).toEqual(['num']);
+    expect(tgTest({})).toEqual(['key', 'num', 'bool']);
+  });
+
+  test('a typeguard test generated can be used in a nested interface', () => {
+    // interface ValueSet {
+    const vsTest = makeTypeGuardTest({
+      value: isString,
+      isTrue: isBoolean,
+    });
+
+    // interface MyFunInterface {
+    const mfiTest = makeTypeGuardTest({
+      key: isString,
+      num: isNumber,
+      bool: isBoolean,
+      valueSet: vsTest,
+    });
+
+    expect(
+      mfiTest({
+        key: 'key',
+        num: 96,
+        bool: false,
+        valueSet: { value: 'hello', isTrue: true },
+      }),
+    ).toEqual([]);
+    expect(
+      mfiTest({
+        key: 'key',
+        num: 96,
+        bool: false,
+        valueSet: { value: 'hello', isTrue: 'true' },
+      }),
+    ).toEqual(['valueSet.isTrue']);
+    expect(
+      mfiTest({
+        key: 'key',
+        num: 96,
+        bool: false,
+        valueSet: { value: true, isTrue: 'true' },
+      }),
+    ).toEqual(['valueSet.value', 'valueSet.isTrue']);
+  });
+
+  test('the value is correctly typeguarded', () => {
+    interface MyFunInterface {
+      key: string;
+      num: number;
+      bool: boolean;
+    }
+    const tg = makeTypeGuardTest({
+      key: isString,
+      num: isNumber,
+      bool: isBoolean,
+    });
+
+    const myVal = {
+      key: 'hello',
+      num: 1,
+      bool: true,
+    };
+
+    const myFunc = (input: MyFunInterface) => (input ? true : false);
+
+    if (tg(myVal)) {
+      expect(myFunc(myVal)).toBe(true);
+    }
+
+    expect.assertions(1);
+  });
+});
---
+++ src/generators/type_guard_generator.ts
@@ -0,0 +1,66 @@
+import { isArray } from '../typeguards/array';
+import { isRecord } from '../typeguards/object';
+
+interface MakeTypeGuardInterface {
+  [key: string]: (input: unknown) => boolean;
+}
+
+type TypeGuardTestInput =
+  | ((input: unknown) => boolean)
+  | ((input: unknown) => string[]);
+
+interface MakeTypeGuardTestInput {
+  [key: string]: TypeGuardTestInput;
+}
+
+/**
+ * Exports a function that can test if an object conforms to a
+ * given interface. The output is an array of strings that
+ * represent the path to the invalid key
+ */
+export function makeTypeGuardTest(
+  input: MakeTypeGuardTestInput,
+): (input: unknown) => string[] {
+  return (valueInput: unknown): string[] => {
+    if (!isRecord(valueInput)) {
+      return ['root'];
+    }
+
+    const outputRaw = Object.entries(input).map(([key, value]) => {
+      const rawResult = value?.(valueInput[key]);
+      if (isArray(rawResult)) {
+        if (rawResult.length === 0) {
+          return undefined;
+        }
+
+        return rawResult.map((value) => `${key}.${value}`);
+      }
+      const result = isArray(rawResult) ? rawResult.length === 0 : rawResult;
+
+      return !result ? key : undefined;
+    });
+
+    const output = outputRaw.flat().filter((value) => value !== undefined);
+
+    return output;
+  };
+}
+
+/**
+ * Exports a function that can test if an object conforms to a
+ * given interface. The output is a boolean and returns the TS
+ * typeguard output. This can be used to type an unknown value.
+ */
+export function makeTypeGuard<T>(
+  input: MakeTypeGuardInterface,
+): (input: unknown) => input is T {
+  return (valueInput: unknown): valueInput is T => {
+    if (!isRecord(valueInput)) {
+      return false;
+    }
+
+    return Object.entries(input).every(
+      ([key, value]) => value?.(valueInput[key]) ?? false,
+    );
+  };
+}
